682022b5b73f6d022388525551c78f84d5233147
Johannes Tegnér <johannes@jitesoft.com>
2017-09-30 13:29:25 +0200

    Added EventListenerInterface.

--- src/Contracts/EventListenerInterface.php
+++ src/Contracts/EventListenerInterface.php
@@ -1,13 +1,21 @@
 <?php
-
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
   EventListenerInterface.php - Part of the woopress project.
 
   © - Jitesoft 2017
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 namespace Jitesoft\wOOPress\Contracts;
 
-class EventListenerInterface {
+/**
+ * Interface for event listeners.
+ */
+interface EventListenerInterface {
 
+    /**
+     * Callback function which will be called when the event that the listener subscribes to is fired.
+     *
+     * @param string $event   Name of the event which invoked the listener.
+     * @param mixed  $args,.. Argument list.
+     */
+    public function invoke(string $event, ...$args);
 }
